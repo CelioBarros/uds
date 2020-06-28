15d7b59ef58fa24293485ef3c7089cb9532a4b12
Célio Barros <celio.barros.filho@gmail.com>
2020-06-28 20:12:27 -0300

    Add acai customizations

--- src/main/java/com/test/uds/domain/Acai.java
+++ src/main/java/com/test/uds/domain/Acai.java
@@ -3,9 +3,11 @@ package com.test.uds.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import org.hibernate.annotations.Cascade;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
@@ -25,4 +27,11 @@ public class Acai {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "size_id", unique = false, nullable = true, insertable = true, updatable = true)
     private Size size;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "acai_has_customization",
+            joinColumns = {@JoinColumn(name = "acai_id")},
+            inverseJoinColumns = {@JoinColumn(name = "customization_id")})
+    private Set<Customization> customizations = new HashSet<>();
+
 }
