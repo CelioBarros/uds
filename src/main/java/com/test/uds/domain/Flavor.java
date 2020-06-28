2a5e0549636af4b89d593ce30e2d9737a136c846
Célio Barros <celio.barros.filho@gmail.com>
2020-06-28 19:18:51 -0300

    Change ids

--- src/main/java/com/test/uds/domain/Flavor.java
+++ src/main/java/com/test/uds/domain/Flavor.java
@@ -13,8 +13,6 @@ import javax.persistence.*;
 public class Flavor {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
-    @SequenceGenerator(name = "sequenceGenerator")
     private Long id;
 
     @Column(name = "name")
--- src/main/java/com/test/uds/domain/Size.java
+++ src/main/java/com/test/uds/domain/Size.java
@@ -13,8 +13,6 @@ import javax.persistence.*;
 public class Size {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
-    @SequenceGenerator(name = "sequenceGenerator")
     private Long id;
 
     @Column(name = "name")
