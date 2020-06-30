fd0c9e24736b3fc027360a67fbc149f44507a224
Célio Barros <celio.barros.filho@gmail.com>
2020-06-29 22:03:59 -0300

    Fix test config

--- src/test/java/com/test/uds/UdsApplicationTests.java
+++ src/test/java/com/test/uds/UdsApplicationTests.java
@@ -1,16 +1,39 @@
-//package com.test.uds;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class UdsApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
-//
-//}
+package com.test.uds;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(initializers = {UdsApplicationTests.Initializer.class})
+public class UdsApplicationTests {
+
+    @ClassRule
+    public static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres")
+            .withDatabaseName("uds")
+            .withUsername("postgres")
+            .withPassword("postgres");
+
+    @Test
+    public void contextLoads() {
+    }
+
+    static class Initializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgres.getJdbcUrl(),
+                    "spring.datasource.username=" + postgres.getUsername(),
+                    "spring.datasource.password=" + postgres.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+}
